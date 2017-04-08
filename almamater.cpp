0de91648e1426a18f970b5801655802c640edc24
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-08 16:14:41 +0700

    update

---
+++ almamater.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    int k,y;
+    y=10;
+    for(k=1;k<=10;k++){
+      printf("%d Nusa Putra Almamaterku %d\n",k,y);
+      y--;}
+      getch();
+      }
---
+++ araybinatang.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    char binatang[5][10]={"kucing","anjing","tikus","biyawak","kadal"};
+    int k,angka[3]={3,5,7};
+    printf("%s\n",binatang[2]);
+    printf("%d\n",angka[2]);
+    getch();}
